75846903c8c9922c1ae8562b9d29d69a0339f38c
Segun Ajala <ajalasegun1@gmail.com>
2023-07-30 17:39:21 +0100

    feat: done

--- App.tsx
+++ App.tsx
@@ -5,12 +5,16 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import RootStack from './src/navigation/RootStack';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaProvider>
       <RootStack />
